8c74c53f9ffe617c1659df93019b0ceb29980231
Elvin <jeendrenal.dev@gmail.com>
2026-01-03 01:45:42 +0800

    camera boundaries/teleport script

--- project/Assets/Script/PlayerControl.cs
+++ project/Assets/Script/PlayerControl.cs
@@ -1,10 +1,42 @@
 using UnityEngine;
+using System.Collections;
+
+// public class PlayerControl : MonoBehaviour
+// {
+//     public float speed = 5f; // Player speed
+//     private Rigidbody2D rb;
+//     private Vector2 moveInput;
+
+//     void Awake()
+//     {
+//         rb = GetComponent<Rigidbody2D>();
+//     }
+
+//     void Update()
+//     {
+//         // Read keyboard input (WASD / arrows)
+//         float moveX = Input.GetAxisRaw("Horizontal");
+//         float moveY = Input.GetAxisRaw("Vertical");
+
+//         moveInput = new Vector2(moveX, moveY).normalized;
+//     }
+
+//     void FixedUpdate()
+//     {
+//         // Move the player
+//         rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
+//     }
+// }
+
 
 public class PlayerControl : MonoBehaviour
 {
     public float speed = 5f; // Player speed
     private Rigidbody2D rb;
     private Vector2 moveInput;
+
+    // Add a flag to stop movement temporarily
+    [HideInInspector] public bool canMove = true;
 
     void Awake()
     {
@@ -13,6 +45,8 @@ public class PlayerControl : MonoBehaviour
 
     void Update()
     {
+        if (!canMove) return; // ignore input if movement is disabled
+
         // Read keyboard input (WASD / arrows)
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
@@ -22,7 +56,21 @@ public class PlayerControl : MonoBehaviour
 
     void FixedUpdate()
     {
-        // Move the player
+        if (!canMove) return; // prevent movement
         rb.MovePosition(rb.position + moveInput * speed * Time.fixedDeltaTime);
     }
+
+    // Call this to disable movement for a delay
+    public void DisableMovement(float delay)
+    {
+        StartCoroutine(EnableMovementAfterDelay(delay));
+    }
+
+    private IEnumerator EnableMovementAfterDelay(float delay)
+    {
+        canMove = false;
+        yield return new WaitForSeconds(delay);
+        canMove = true;
+    }
 }
+
---
+++ project/Assets/Script/Teleport.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+public class TeleportBlink : MonoBehaviour
+{
+    public Vector2 teleportPosition;
+    public int blinkCount = 3;
+    public float blinkSpeed = 0.1f;
+    public float walkDelay = 1f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Teleport player
+            other.transform.position = teleportPosition;
+
+            // Stop player movement temporarily
+            PlayerControl pc = other.GetComponent<PlayerControl>();
+            if (pc != null)
+            {
+                pc.DisableMovement(walkDelay + blinkCount * blinkSpeed * 2);
+            }
+
+            // Blink effect
+            SpriteRenderer sr = other.GetComponent<SpriteRenderer>();
+            if (sr != null)
+            {
+                StartCoroutine(BlinkPlayer(sr));
+            }
+        }
+    }
+
+    private IEnumerator BlinkPlayer(SpriteRenderer sr)
+    {
+        for (int i = 0; i < blinkCount * 2; i++)
+        {
+            sr.enabled = !sr.enabled;
+            yield return new WaitForSeconds(blinkSpeed);
+        }
+        sr.enabled = true;
+    }
+}
