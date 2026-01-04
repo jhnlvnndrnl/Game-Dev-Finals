b92dc9f2d581a034ba99dcd3b916d0df459bf254
Elvin <jeendrenal.dev@gmail.com>
2026-01-05 00:27:34 +0800

    player & water animation

--- project/Assets/Script/PlayerControl.cs
+++ project/Assets/Script/PlayerControl.cs
@@ -32,9 +32,10 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour
 {
     public float speed = 5f; // Player speed
+    public int facingDirection = 1;
     private Rigidbody2D rb;
     private Vector2 moveInput;
-
+    // public Animator anim;
     // Add a flag to stop movement temporarily
     [HideInInspector] public bool canMove = true;
 
@@ -45,13 +46,22 @@ public class PlayerControl : MonoBehaviour
 
     void Update()
     {
+        // Read keyboard input (WASD / arrows)
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if(horizontal > 0 && transform.localScale.x < 0 || 
+        horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
         if (!canMove) return; // ignore input if movement is disabled
 
-        // Read keyboard input (WASD / arrows)
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        // anim.SetFloat("horizontal", horizontal);
+        // anim.SetFloat("vertical", vertical);
 
-        moveInput = new Vector2(moveX, moveY).normalized;
+        moveInput = new Vector2(horizontal, vertical).normalized;
     }
 
     void FixedUpdate()
@@ -72,4 +82,14 @@ public class PlayerControl : MonoBehaviour
         yield return new WaitForSeconds(delay);
         canMove = true;
     }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
 }
